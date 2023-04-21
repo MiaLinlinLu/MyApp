dc1b48db3a802d29266078e0a36984847f0f9af6
Linlin Lu <pku600@126.com>
2023-04-20 23:55:11 -0400

    record events app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,85 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./styles.css";
 
-let name: string;
+type Event = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
-name = "Mia";
+const EventLog = () => {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [newEventTitle, setNewEventTitle] = useState<string>("");
 
-function App() {
+  const handleAddEvent = () => {
+    if (newEventTitle.trim() !== "") {
+      const newEvent: Event = {
+        id: Date.now(),
+        title: newEventTitle,
+        completed: false,
+      };
+      setEvents([...events, newEvent]);
+      setNewEventTitle("");
+    }
+  };
+
+  const handleDeleteEvent = (id: number) => {
+    const updatedEvents = events.filter((event) => event.id !== id);
+    setEvents(updatedEvents);
+  };
+
+  const handleMarkEventCompleted = (id: number) => {
+    const updatedEvents = events.map((event) => {
+      if (event.id === id) {
+        return {
+          ...event,
+          completed: true,
+        };
+      } else {
+        return event;
+      }
+    });
+    setEvents(updatedEvents);
+  };
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Hello {name}.</p>
-      </header>
+    <div className='container'>
+      <div className='landing-page'>
+        <h1>Welcome to the Event Log!</h1>
+        <p>
+          This is a simple event log app built with React and TypeScript. You
+          can use it to keep track of your upcoming events.
+        </p>
+      </div>
+      <div className='event-log'>
+        <h2>Add Event</h2>
+        <div>
+          <input
+            type='text'
+            value={newEventTitle}
+            onChange={(e) => setNewEventTitle(e.target.value)}
+          />
+          <button onClick={handleAddEvent}>Add Event</button>
+        </div>
+        <h2>Events</h2>
+        <ul>
+          {events.map((event) => (
+            <li key={event.id}>
+              <span>{event.title}</span>
+              <button onClick={() => handleDeleteEvent(event.id)}>
+                Delete
+              </button>
+              {!event.completed && (
+                <button onClick={() => handleMarkEventCompleted(event.id)}>
+                  Mark as Completed
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default EventLog;
